0676f0328f19726156ba1eb614488e22f7251100
nrally <nrally@ncsu.edu>
2026-08-19 14:45:46 -0400

    Push changes to box

--- drawstuff.js
+++ drawstuff.js
@@ -78,9 +78,9 @@ function main() {
     var imagedata = context.createImageData(w,h);
  
     // Draw a rectangle with pixels
-    var c = new Color(0,0,0,255); // the color at the pixel: black opaque
-    for (var x=50; x<100; x++) 
-        for (var y=50; y<75; y++) {
+    var c = new Color(125,125,125,255); // the color at the pixel: black opaque
+    for (var x=100; x<250; x++) 
+        for (var y=100; y<250; y++) {
             drawPixel(imagedata,x,y,c);
             // console.log("draw at " +x+ " " +y);
         }
